7efa7c628b6ac6557c3781971ea5b3f9370c19b4
bacon🥓 <ngoclocta6@gmail.com>
2025-11-24 10:49:12 +0700

    Add input validation for positive numbers

---
+++ web_xanh_firebase/bai1.py
@@ -0,0 +1,7 @@
+a=float(input("nhap so a:"))
+b=float(input("nhap so b:"))
+c=float(input("nhap so c:"))
+if (a > 0) and (b > 0) and (c > 0):
+    print("a, b, c la so duong")
+else:
+    print("Một trong ba số khong phai so duong")
